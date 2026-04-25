df21cca180462733ab5a29fea6662239788a8b0b
udaykumar <udaykumarsamidala57@gmail.com>
2026-04-25 18:40:24 +0530

    updated changes

--- src/main/java/com/controller/Admissions/AdmissionServlet.java
+++ src/main/java/com/controller/Admissions/AdmissionServlet.java
@@ -20,8 +20,9 @@ public class AdmissionServlet extends HttpServlet {
         	        + "applicant_name, date_of_birth, gender, Admission_type, "
         	        + "native_place, taluk, district, state, nationality, "
         	        + "religion_category, category, cast, mother_tongue, blood_group, "
-        	        + "father_guardian_name,father_occupation,Father_org, mother_name, mother_occupation,Mother_org, income, "
-        	        + "postal_address, permanent_address, phone_no,Whatsapp_no, email, aadhar_no, `APAAR ID`, "
+        	        + "father_guardian_name, father_occupation, Father_org, "
+        	        + "mother_name, mother_occupation, Mother_org, income, "
+        	        + "postal_address, permanent_address, phone_no, email, aadhar_no, `APAAR ID`, "
         	        + "medium_of_instruction, sscl_passing_year, SSLC_Board, SSLC_TMarks, "
         	        + "marks_maths, marks_science, "
         	        + "preference_1, preference_2, preference_3, preference_4, preference_5"
@@ -52,30 +53,30 @@ public class AdmissionServlet extends HttpServlet {
         	ps.setString(18, request.getParameter("mother_name"));
         	ps.setString(19, request.getParameter("mother_occupation"));
         	ps.setString(20, request.getParameter("Mother_org"));
+
         	ps.setDouble(21, parseDouble(request.getParameter("income")));
 
         	ps.setString(22, request.getParameter("postal_address"));
         	ps.setString(23, request.getParameter("permanent_address"));
         	ps.setString(24, request.getParameter("phone_no"));
-        	ps.setString(24, request.getParameter("Whatsapp_no"));
-        	ps.setString(26, request.getParameter("email"));
-        	ps.setString(27, request.getParameter("aadhar_no"));
-        	ps.setString(28, request.getParameter("APAAR"));
+        	ps.setString(25, request.getParameter("email"));
+        	ps.setString(26, request.getParameter("aadhar_no"));
+        	ps.setString(27, request.getParameter("APAAR"));
 
-        	ps.setString(29, request.getParameter("medium_of_instruction"));
-        	ps.setString(30, request.getParameter("sscl_passing_year"));
+        	ps.setString(28, request.getParameter("medium_of_instruction"));
+        	ps.setString(29, request.getParameter("sscl_passing_year"));
 
-        	ps.setString(31, request.getParameter("SSLC_Board"));   // FIXED
-        	ps.setString(32, request.getParameter("SSLC_TMarks"));  // FIXED
+        	ps.setString(30, request.getParameter("SSLC_Board"));
+        	ps.setString(31, request.getParameter("SSLC_TMarks"));
 
-        	ps.setDouble(33, parseDouble(request.getParameter("marks_maths")));
-        	ps.setDouble(34, parseDouble(request.getParameter("marks_science")));
+        	ps.setDouble(32, parseDouble(request.getParameter("marks_maths")));
+        	ps.setDouble(33, parseDouble(request.getParameter("marks_science")));
 
-        	ps.setString(35, request.getParameter("preference_1"));
-        	ps.setString(36, request.getParameter("preference_2"));
-        	ps.setString(37, request.getParameter("preference_3"));
-        	ps.setString(38, request.getParameter("preference_4"));
-        	ps.setString(39, request.getParameter("preference_5"));
+        	ps.setString(34, request.getParameter("preference_1"));
+        	ps.setString(35, request.getParameter("preference_2"));
+        	ps.setString(36, request.getParameter("preference_3"));
+        	ps.setString(37, request.getParameter("preference_4"));
+        	ps.setString(38, request.getParameter("preference_5"));
 
             ps.executeUpdate();
 
