ed6c895570d421e0085c272e0142cacd5ac5e367
udaykumar <udaykumarsamidala57@gmail.com>
2026-04-28 18:26:41 +0530

    updated changes

--- src/main/java/com/controller/Admissions/Enquiries.java
+++ src/main/java/com/controller/Admissions/Enquiries.java
@@ -207,7 +207,7 @@ public class Enquiries extends HttpServlet {
             e.printStackTrace();
         }
 
-        response.sendRedirect("AdmissionListServlet");
+        response.sendRedirect("Enquiries");
     }
 
     // ========================= HELPERS =========================
